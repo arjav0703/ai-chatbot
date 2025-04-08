10fbe145bba720fd702928d72fc81f7340b6bce2
Arjav Jain <arjavjain0703@gmail.com>
2025-04-08 17:14:39 +0530

    modify parameters

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,44 @@
 "use client";
-import { useEffect } from 'react';
-import '@n8n/chat/style.css';
-import { createChat } from '@n8n/chat';
-
+import { useEffect } from "react";
+import "@n8n/chat/style.css";
+import { createChat } from "@n8n/chat";
 
 export default function App() {
-	useEffect(() => {
-		createChat({
-			webhookUrl: 'https://n8n.arjav.hackclub.app/webhook/0d26c855-4594-4a4b-8f83-e2f062ff051d/chat',
+  useEffect(() => {
+    createChat({
+      webhookUrl:
+        "https://n8n.arjav.hackclub.app/webhook/0d26c855-4594-4a4b-8f83-e2f062ff051d/chat",
       webhookConfig: {
-        method: 'POST',
-        headers: {}
+        method: "POST",
+        headers: {},
       },
-      target: '#n8n-chat',
-      mode: 'fullscreen',
-      chatInputKey: 'chatInput',
-      chatSessionKey: 'sessionId',
+      target: "#n8n-chat",
+      mode: "fullscreen",
+      chatInputKey: "chatInput",
+      chatSessionKey: "sessionId",
       metadata: {},
       showWelcomeScreen: false,
-      defaultLanguage: 'en',
+      defaultLanguage: "en",
       initialMessages: [
-        'Hi there! 👋',
-        'I am Chemi. I am here to help you with your questions.'
+        "Hi there! 👋",
+        "I am Chemi. I am here to help you with your questions.",
       ],
       i18n: {
         en: {
-          title: 'Hi there! 👋',
-          subtitle: "Start a chat. We're here to help you 24/7.",
-          footer: '',
-          getStarted: 'New Conversation',
-          inputPlaceholder: 'Type your question..',
-          closeButtonTooltip: 'Close chat',
+          title: "Class 9",
+          subtitle: "",
+          footer: "",
+          getStarted: "New Conversation",
+          inputPlaceholder: "Type your question..",
+          closeButtonTooltip: "Close chat",
         },
       },
+    });
+  }, []);
 
-		});
-	}, []);
-
-	return (
-    <div className='justify-end'>
-      <div id="n8n-chat" className='w-4/5'/>
+  return (
+    <div className="justify-end">
+      <div id="n8n-chat" className="w-4/5" />
     </div>
   );
-};
+}
