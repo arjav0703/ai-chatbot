bdb83f5fd3eb12b74d18a54fb7eb2bfa3921261e
Arjav Jain <arjavjain0703@gmail.com>
2025-04-09 15:47:25 +0530

    add google oauth2 creds

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import SignIn from "./components/SignIn";
+
 export default function Home() {
   return (
     <>
       <div>
         <h1>CBSE AI</h1>
+        <SignIn />
       </div>
     </>
   );
