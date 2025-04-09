937b1b9f0118c79087efe3127d26d03c1136a35f
Arjav Jain <arjavjain0703@gmail.com>
2025-04-09 15:26:43 +0530

    init authja

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,9 @@
-"use client";
-import { useEffect } from "react";
-import "@n8n/chat/style.css";
-import { createChat } from "@n8n/chat";
-export default function App() {
-  useEffect(() => {
-    createChat({
-      webhookUrl:
-        "https://n8n.arjav.hackclub.app/webhook/79f0a176-56bf-41e7-aeba-338288130bde/chat",
-      webhookConfig: {
-        method: "POST",
-        headers: {},
-      },
-      target: "#n8n-chat",
-      mode: "fullscreen",
-      chatInputKey: "chatInput",
-      chatSessionKey: "sessionId",
-      metadata: {},
-      showWelcomeScreen: false,
-      defaultLanguage: "en",
-      initialMessages: [
-        "Hi! 👋",
-        "I am Chemi. I am here to help you with your questions.",
-      ],
-      i18n: {
-        en: {
-          title: "Class 9",
-          subtitle: "",
-          footer: "",
-          getStarted: "New Conversation",
-          inputPlaceholder: "Ask chemi",
-          closeButtonTooltip: "Close chat",
-        },
-      },
-    });
-  }, []);
-
+export default function Home() {
   return (
-    <div className="h-screen">
-      <div id="n8n-chat" className="h-full w-full" />
-    </div>
+    <>
+      <div>
+        <h1>CBSE AI</h1>
+      </div>
+    </>
   );
 }
