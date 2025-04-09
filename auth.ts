cd4a0e74ba00d8d57ce2489d5dac23ba95ce2145
Arjav Jain <arjavjain0703@gmail.com>
2025-04-09 18:19:39 +0530

    auth works!

--- auth.ts
+++ auth.ts
@@ -3,10 +3,4 @@ import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
-  callbacks: {
-    authorized: async ({ auth }) => {
-      // Logged in users are authenticated, otherwise redirect to login page
-      return !!auth;
-    },
-  },
 });
--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,19 @@
-export { auth as middleware } from "@/auth";
+import { auth } from "@/auth";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+// Export the middleware function
+export default auth((req) => {
+  const { pathname } = req.nextUrl;
+
+  // If the user is not authenticated and trying to access a protected route
+  if (!req.auth && pathname !== "/") {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+  return NextResponse.next();
+});
+
+// Configure which paths the middleware should run on
+export const config = {
+  matcher: ["/chat"],
+};
