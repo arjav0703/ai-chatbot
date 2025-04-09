81cda8f04cce2d113533fb7060a9aa5e28192f0e
Arjav Jain <arjavjain0703@gmail.com>
2025-04-09 16:33:08 +0530

    protect with middleware

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-import SignIn from "./components/SignIn";
+import SignIn from "@/components/auth/SignIn";
 
 export default function Home() {
   return (
     <>
-      <div>
-        <h1>CBSE AI</h1>
-        <SignIn />
+      <div className="flex flex-wrap bg-zinc-900 h-screen justify-center items-center">
+        <div className="text-center dark">
+          <h1 className="text-white text-6xl mb-12">CBSE AI</h1>
+          <SignIn />
+        </div>
       </div>
     </>
   );
--- components/auth/SignIn.tsx
+++ components/auth/SignIn.tsx
@@ -1,14 +1,14 @@
 import { signIn } from "@/auth";
-
+import { Button } from "@/components/ui/button";
 export default function SignIn() {
   return (
     <form
       action={async () => {
         "use server";
-        await signIn("google");
+        await signIn("google", { redirectTo: "/chat" });
       }}
     >
-      <button type="submit">Signin with Google</button>
+      <Button type="submit">Sign in with Google</Button>
     </form>
   );
 }
