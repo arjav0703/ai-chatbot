fc108e925bbf0485fad5d0a7389f5188bb652f2e
Arjav Jain <arjavjain0703@gmail.com>
2025-04-11 13:21:07 +0530

    fix redirect uri

--- components/auth/SignIn.tsx
+++ components/auth/SignIn.tsx
@@ -5,7 +5,7 @@ export default function SignIn() {
     <form
       action={async () => {
         "use server";
-        await signIn("google", { redirectTo: "/chat" });
+        await signIn("google", { redirectTo: "/" });
       }}
     >
       <Button type="submit">Sign in with Google</Button>
