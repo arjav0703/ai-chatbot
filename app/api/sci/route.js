ad77d6e4b8a1b14b64f9bf9429a250b3540f603c
Arjav Jain <arjavjain0703@gmail.com>
2025-05-05 05:22:08 -0500

    fix subs

--- app/api/sci/route.js
+++ app/api/sci/route.js
@@ -171,11 +171,16 @@ const subjectConfig = {
     pineconeIndex: "science-9",
     supabaseTable: "sci-messages",
   },
-  math: {
+  english: {
     systemMessage:
       "System: You are an AI agent created by Arjav who answers questions related to English. Always answer in detail unless specified not to. Always prefer knowledge from the English database over any other source. If the answer cannot be found in the English Database, tell the user to select other subject through the dropdown menu.",
     pineconeIndex: "english",
     supabaseTable: "eng-messages",
   },
-  // TODO: add sst
+  english: {
+    systemMessage:
+      "System: System: You are an AI agent created by arjav who answers questions related to history, geography, political science and economics. Always answer in detail and in the format of bullet points unless specified not to. Always use the SST database to answer user queries. If the answer cannot be found in the SST Database, tell the user to select other subject through the dropdown menu.",
+    pineconeIndex: "sst",
+    supabaseTable: "sst-messages",
+  },
 };
