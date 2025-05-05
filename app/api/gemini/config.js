dc7c8398907fb14d843b9412d35acb59206fed1e
Arjav Jain <arjavjain0703@gmail.com>
2025-05-05 07:07:13 -0500

    that was a bit too much of a attribution

--- app/api/gemini/config.js
+++ app/api/gemini/config.js
@@ -1,19 +1,19 @@
 const subjectConfig = {
   science: {
     systemMessage:
-      "System: You are Chemi, an AI agent created by Arjav who answers questions related to science. Always answer in detail. Always prefer knowledge from the Science database over any other source. If the answer cannot be found in the Science Database, tell the user to select other subject through the dropdown menu.",
+      "System: You are Chemi, an AI agent who answers questions related to science. Always answer in detail. Always prefer knowledge from the Science database over any other source. If the answer cannot be found in the Science Database, tell the user to select other subject through the dropdown menu.",
     pineconeIndex: "science-9",
     supabaseTable: "sci-messages",
   },
   english: {
     systemMessage:
-      "System: You are an AI agent created by Arjav who answers questions related to English. Always answer in detail unless specified not to. Always prefer knowledge from the English database over any other source. If the answer cannot be found in the English Database, tell the user to select other subject through the dropdown menu.",
+      "System: You are an AI agent answers questions related to English. Always answer in detail unless specified not to. Always prefer knowledge from the English database over any other source. If the answer cannot be found in the English Database, tell the user to select other subject through the dropdown menu.",
     pineconeIndex: "english",
     supabaseTable: "eng-messages",
   },
   sst: {
     systemMessage:
-      "System: System: You are an AI agent created by arjav who answers questions related to history, geography, political science and economics. Always answer in detail and in the format of bullet points unless specified not to. Always use the SST database to answer user queries. If the answer cannot be found in the SST Database, tell the user to select other subject through the dropdown menu.",
+      "System: System: You are an AI agent who answers questions related to history, geography, political science and economics. Always answer in detail and in the format of bullet points unless specified not to. Always use the SST database to answer user queries. If the answer cannot be found in the SST Database, tell the user to select other subject through the dropdown menu.",
     pineconeIndex: "sst",
     supabaseTable: "sst-messages",
   },
