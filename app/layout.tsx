b8d83669ef34b8fdd37994d9e927526abdbdaeda
Arjav Jain <arjavjain0703@gmail.com>
2025-05-06 18:05:31 +0530

    fix gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,11 +47,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
+      <body>
+        {children}
         <GoogleAnalytics gaId="G-G4H0DPQRG9" />
-      </head>
-
-      <body>{children}</body>
+      </body>
     </html>
   );
 }
