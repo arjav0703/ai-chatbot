ca7a73f1c8ed2f5a0619de34e76bf30a6a5b6a7a
Arjav Jain <arjavjain0703@gmail.com>
2025-05-06 10:30:18 +0530

    remove unused code

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,19 +40,6 @@ export const metadata: Metadata = {
   },
 };
 
-// <Head>
-//     <title>Home</title>
-//     <Meta
-//     as={Head} // component to wrap tags in, defaults to React.Fragment
-//     name="Hack Club" // site name
-//     title="Home" // page title
-//     description="Hackclub Vidisha is a high school coding club in vidisha" // page description
-//     image="https://hackathons.hackclub.com/card.png" // large summary image URL
-//     color="#ec3750" // theme color
-//     manifest="/site.webmanifest" />
-//     <meta name="google-site-verification" content="hThcfgfWeBT7H0Qp2yp934ergwCeAWIq8y9y9gIXqE8" />
-// </Head>
-
 export default function RootLayout({
   children,
 }: Readonly<{
