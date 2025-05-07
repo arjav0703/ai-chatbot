1ccd18b8435a0fdaccfb53699e9862a9e3eeef1f
Arjav Jain <arjavjain0703@gmail.com>
2025-05-07 22:14:21 +0530

    fix eslint err

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useEffect, useState } from "react";
-import { ChevronDown } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import ChatNav from "@/components/ChatNav";
 import {
