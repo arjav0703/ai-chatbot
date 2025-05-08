63d6b6876b6eb257ca75e2d732fe0ec7ec332f32
Arjav Jain <arjavjain0703@gmail.com>
2025-05-08 17:34:13 +0530

    update 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,38 +1,41 @@
 import Link from "next/link";
+import "./notfound.css";
 
 export default function NotFound() {
   return (
-    <div className="dark">
-      <h1>
-        Ahoy Sailor! Looks like ye are stranded alone on this island of lost
-        pages...
-      </h1>
-      <p>
-        This be no place for the likes of you — just tumbleweeds, broken links,
-        and echoes of forgotten treasure maps. The page ye be seekin’ has
-        vanished into the depths of Davy Jones` locker. fear not! Hoist yer
-        sails and steer back to safer shores:
-      </p>
-      <ul>
-        <li>
-          * ⚓
-          <Link href="/" className="text-blue-600">
-            Home Port
-          </Link>
-        </li>
-        <li>
-          * 🧭
-          <Link href="/about" className="text-blue-600">
-            Explore
-          </Link>
-        </li>
-        <li>
-          * 🦜
-          <Link href="/whoami" className="text-blue-600">
-            Have a look at your boat
-          </Link>
-        </li>
-      </ul>
-    </div>
+    <>
+      <div className="dark max-w-4xl mx-auto my-30">
+        <h1>
+          Ahoy Sailor! Looks like ye are stranded alone on this island of lost
+          pages...
+        </h1>
+        <p>
+          This be no place for the likes of you — just tumbleweeds, broken
+          links, and echoes of forgotten treasure maps. The page ye be seekin’
+          has vanished into the depths of Davy Jones` locker. fear not! Hoist
+          yer sails and steer back to safer shores:
+        </p>
+        <ul>
+          <li>
+            * ⚓
+            <Link href="/" className="text-blue-600">
+              Home Port
+            </Link>
+          </li>
+          <li>
+            * 🧭
+            <Link href="/about" className="text-blue-600">
+              Explore
+            </Link>
+          </li>
+          <li>
+            * 🦜
+            <Link href="/whoami" className="text-blue-600">
+              Have a look at your boat
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 }
