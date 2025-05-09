1ac5c13ba0149eadb83eb74ef9a6b407494299da
Arjav Jain <arjavjain0703@gmail.com>
2025-05-09 18:20:49 +0530

    mobile opt

--- components/ChatNav.tsx
+++ components/ChatNav.tsx
@@ -8,7 +8,7 @@ export default function ChatNav() {
     <nav className="flex justify-between items-center w-full mb-4">
       <Link
         href="/"
-        className="text-l lg:text-2xl font-bold tars-mono flex items-center gap-2"
+        className="text-l lg:text-2xl font-bold tars-mono items-center gap-2 hidden lg:flex"
       >
         <ArrowLeft className="w-6 h-6" />
         <span className="hidden lg:flex">CBSE-AI</span>
