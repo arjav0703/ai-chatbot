513acafafa203d8ab0ea614f4c9df6d1bce5f822
arjav0703 <arjavjain0703@gmail.com>
2025-05-11 22:19:31 +0530

    update adapter

--- auth.ts
+++ auth.ts
@@ -1,13 +1,30 @@
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
+import { SupabaseAdapter } from "@auth/supabase-adapter"
+import { createClient } from '@supabase/supabase-js';
+// import { MongoClient } from "mongodb";
+// import { MongoDBAdapter } from "@auth/mongodb-adapter";
 
-import { MongoClient } from "mongodb";
-import { MongoDBAdapter } from "@auth/mongodb-adapter";
+// const client = new MongoClient(process.env.MONGODB_URI!);
 
-const client = new MongoClient(process.env.MONGODB_URI!);
+// export const { handlers, auth, signIn, signOut } = NextAuth({
+  // adapter: MongoDBAdapter(client),
+  // session: { strategy: "jwt" },
+  // ...authConfig,
+// });
 
+// import NextAuth from "next-auth";
+// import authConfig from "./auth.config";
+const supabaseUrl = process.env.SUPABASE_URL!;
+const supabaseKey = process.env.SUPABASE_KEY!;
+const supabase = createClient(supabaseUrl, supabaseKey);
+ 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter: MongoDBAdapter(client),
-  session: { strategy: "jwt" },
   ...authConfig,
-});
+  adapter: SupabaseAdapter({
+    url: supabaseUrl,
+    secret: supabaseKey,
+  }),
+  secret: process.env.NEXT_AUTH_SEC
+})
+
