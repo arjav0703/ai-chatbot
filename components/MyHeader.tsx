fea03da2e76841d7b69f1805f65db383d3a716e3
arjav0703 <arjavjain0703@gmail.com>
2025-05-14 15:04:24 +0530

    Revert "update header"
    This reverts commit 0cf48cb1e5642864fff26b4075b81e6d1e132b93.

--- components/MyHeader.tsx
+++ components/MyHeader.tsx
@@ -3,17 +3,14 @@ import { Button } from "@/components/ui/button";
 
 export default function Myheader() {
   return (
-    <header className="sm:px-8 px-6 py-6 flex justify-between items-center backdrop-blur-sm bg-black/5 sticky top-0 z-50 border-b border-white/10">
+    <header className="sm:p-6 p-4 flex justify-between items-center">
       <Link href="/">
-        <h1 className="sm:text-2xl text-xl font-bold text-white tracking-tight">
+        <h1 className="sm:text-3xl text-2xl font-bold text-white tars-mono">
           CBSE AI
         </h1>
       </Link>
       <Link href="/login">
-        <Button
-          variant="outline"
-          className="text-white border-white/70 hover:bg-white/10 hover:text-white transition-all"
-        >
+        <Button variant="outline" className="text-white border-white ">
           Sign In
         </Button>
       </Link>
